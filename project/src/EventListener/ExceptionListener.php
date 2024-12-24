ceee8430c5dbaf81075810365666b7ba20a4f1d5
M-CRM1 <megabit.crm@gmail.com>
2024-12-24 10:42:50 +0500

    add validation listener

--- project/src/EventListener/ExceptionListener.php
+++ project/src/EventListener/ExceptionListener.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 #[AsEventListener(event: 'kernel.exception')]
 class ExceptionListener
@@ -15,19 +16,37 @@ class ExceptionListener
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-        $message = $exception->getMessage();
+        $response = new JsonResponse();
 
-        $response = new JsonResponse(['message' => $message]);
+        if ($exception instanceof ValidationFailedException || $exception->getPrevious() instanceof ValidationFailedException) {
+            $validationFailedException = ($exception instanceof ValidationFailedException)
+                ? $exception
+                : $exception->getPrevious();
 
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } elseif ($exception instanceof EntityNotFoundException) {
-            $response->setStatusCode($exception->getCode());
+            $errors = [];
+            foreach ($validationFailedException->getViolations() as $violation) {
+                $errors[] = [
+                    'path' => $violation->getPropertyPath(),
+                    'error' => $violation->getMessage()
+                ];
+            }
+            
+            $response->setData($errors);
+            $response->setStatusCode(422);
         } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+            $message = $exception->getMessage();
+            $response->setData(['message' => $message]);
 
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+            } elseif ($exception instanceof EntityNotFoundException) {
+                $response->setStatusCode($exception->getCode());
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+        
         $event->setResponse($response);
     }
 }
