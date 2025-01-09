7b1cca15d3d49929f54a47b16b5c343a46b3c6ca
megabite-work <g.work.01.01.2000@gmail.com>
2025-01-09 14:50:29 +0500

    refactor: streamline CreateAction by replacing entity creation logic with array_map for improved readability

--- project/src/Action/Unit/CreateAction.php
+++ project/src/Action/Unit/CreateAction.php
@@ -3,10 +3,7 @@
 namespace App\Action\Unit;
 
 use App\Component\EntityNotFoundException;
-<<<<<<< HEAD
 use App\Dto\Unit\IndexDto;
-=======
->>>>>>> b6d1ea7 (feat: add DTO classes for various entities and implement error handling)
 use App\Dto\Unit\RequestDto;
 use App\Entity\Unit;
 use App\Repository\UnitRepository;
@@ -22,21 +19,9 @@ class CreateAction
 
     public function __invoke(array $dtos): array
     {
-<<<<<<< HEAD
         try {
             $this->em->beginTransaction();
             $data = array_map(fn(RequestDto $dto): IndexDto => $this->create($dto), $dtos);
-=======
-        $this->em->beginTransaction();
-        $entities = [];
-
-        try {
-            foreach ($dtos as $dto) {
-                $entity = $this->create($dto);
-                $entities[] = $entity;
-            }
-
->>>>>>> b6d1ea7 (feat: add DTO classes for various entities and implement error handling)
             $this->em->flush();
             $this->em->commit();
         } catch (\Throwable $th) {
@@ -44,7 +29,6 @@ class CreateAction
             throw new EntityNotFoundException($th->getMessage(), $th->getCode());
         }
 
-<<<<<<< HEAD
         return $data;
     }
 
@@ -61,24 +45,5 @@ class CreateAction
         }
 
         return IndexDto::fromEntity($entity);
-=======
-        return $entities;
-    }
-
-    private function create(RequestDto $dto): Unit
-    {
-        $unit = $this->unitRepository->findOneBy(['code' => $dto->getCode()]);
-
-        if (!$unit) {
-            $unit = (new Unit())
-                ->setName($dto->getName())
-                ->setCode($dto->getCode())
-                ->setIcon($dto->getIcon());
-
-            $this->em->persist($unit);
-        }
-
-        return $unit;
->>>>>>> b6d1ea7 (feat: add DTO classes for various entities and implement error handling)
     }
 }
