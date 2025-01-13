f66b59b2793cc039064c2173de9c473d090d340e
megabite-work <g.work.01.01.2000@gmail.com>
2025-01-13 15:15:58 +0500

    fix: comment out unused RequestBody annotation in WebCredentialController

--- src/Controller/WebCredentialController.php
+++ src/Controller/WebCredentialController.php
@@ -43,50 +43,50 @@ class WebCredentialController extends AbstractController
     }
 
     #[Route('/{multi_store_id<\d+>}', methods: ['PATCH'])]
-    #[OA\RequestBody(
-        description: 'Your request body description',
-        content: new OA\JsonContent(
-            properties: [
-                new OA\Property(property: 'category', type: 'string', example: 'category'),
-                new OA\Property(
-                    property: 'secrets',
-                    type: 'object',
-                    properties: [
-                        new OA\Property(
-                            property: 'instagram',
-                            type: 'object',
-                            properties: [
-                                new OA\Property(property: 'login', type: 'string', example: 'login'),
-                                new OA\Property(property: 'password', type: 'string', example: 'secret'),
-                            ]
-                        ),
-                    ]
-                ),
-                new OA\Property(
-                    property: 'social',
-                    type: 'object',
-                    properties: [
-                        new OA\Property(
-                            property: 'telegram',
-                            type: 'object',
-                            properties: [
-                                new OA\Property(property: 'url', type: 'string', example: 't.me/url'),
-                                new OA\Property(property: 'isActive', type: 'boolean', example: true),
-                            ]
-                        ),
-                        new OA\Property(
-                            property: 'facebook',
-                            type: 'object',
-                            properties: [
-                                new OA\Property(property: 'url', type: 'string', example: 'facebook.com/url'),
-                                new OA\Property(property: 'isActive', type: 'boolean', example: true),
-                            ]
-                        ),
-                    ]
-                ),
-            ]
-        )
-    )]
+    // #[OA\RequestBody(
+    //     description: 'Your request body description',
+    //     content: new OA\JsonContent(
+    //         properties: [
+    //             new OA\Property(property: 'category', type: 'string', example: 'category'),
+    //             new OA\Property(
+    //                 property: 'secrets',
+    //                 type: 'object',
+    //                 properties: [
+    //                     new OA\Property(
+    //                         property: 'instagram',
+    //                         type: 'object',
+    //                         properties: [
+    //                             new OA\Property(property: 'login', type: 'string', example: 'login'),
+    //                             new OA\Property(property: 'password', type: 'string', example: 'secret'),
+    //                         ]
+    //                     ),
+    //                 ]
+    //             ),
+    //             new OA\Property(
+    //                 property: 'social',
+    //                 type: 'object',
+    //                 properties: [
+    //                     new OA\Property(
+    //                         property: 'telegram',
+    //                         type: 'object',
+    //                         properties: [
+    //                             new OA\Property(property: 'url', type: 'string', example: 't.me/url'),
+    //                             new OA\Property(property: 'isActive', type: 'boolean', example: true),
+    //                         ]
+    //                     ),
+    //                     new OA\Property(
+    //                         property: 'facebook',
+    //                         type: 'object',
+    //                         properties: [
+    //                             new OA\Property(property: 'url', type: 'string', example: 'facebook.com/url'),
+    //                             new OA\Property(property: 'isActive', type: 'boolean', example: true),
+    //                         ]
+    //                     ),
+    //                 ]
+    //             ),
+    //         ]
+    //     )
+    // )]
     public function update(int $multiStoreId, #[MapRequestPayload(serializationContext: ['groups' => ['web_credential:update']])] RequestDto $dto, UpdateAction $action): JsonResponse
     {
         $this->existsValidate($multiStoreId, MultiStore::class);
