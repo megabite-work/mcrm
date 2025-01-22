f5b4f01bdde291c72176d809df389162e253b259
megabite-work <g.work.01.01.2000@gmail.com>
2025-01-22 18:06:35 +0500

    fix: remove unused NotFoundHttpException import in ExceptionListener for cleaner code

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 
