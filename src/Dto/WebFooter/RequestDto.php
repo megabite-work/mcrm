82221e17901e77e14671d9ba74afee1a721fcbc6
megabite-work <g.work.01.01.2000@gmail.com>
2025-01-27 11:19:17 +0500

    fix: rename isActive field to is_active in RequestDto for consistency

--- src/Dto/WebFooter/RequestDto.php
+++ src/Dto/WebFooter/RequestDto.php
@@ -29,7 +29,7 @@ final class RequestDto
                     fields: [
                         'title' => [new Assert\NotBlank(groups: ['web_footer:create', 'web_footer:update']), new Assert\Type(type: 'string', groups: ['web_footer:create', 'web_footer:update'])],
                         'type' => [new Assert\NotBlank(groups: ['web_footer:create', 'web_footer:update']), new Assert\Type(type: 'string', groups: ['web_footer:create', 'web_footer:update'])],
-                        'isActive' => [new Assert\NotBlank(groups: ['web_footer:create', 'web_footer:update']), new Assert\Type(type: 'boolean', groups: ['web_footer:create', 'web_footer:update'])],
+                        'is_active' => [new Assert\NotBlank(groups: ['web_footer:create', 'web_footer:update']), new Assert\Type(type: 'boolean', groups: ['web_footer:create', 'web_footer:update'])],
                         'url' => [new Assert\NotBlank(groups: ['web_footer:create', 'web_footer:update']), new Assert\Url(groups: ['web_footer:create', 'web_footer:update'])],
                     ],
                     groups: ['web_footer:create', 'web_footer:update']
