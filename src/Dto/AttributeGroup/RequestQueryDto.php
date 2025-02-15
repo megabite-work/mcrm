59b785f43d3089bcb74cd95b0b4f90fe945552ee
megabite-work <g.work.01.01.2000@gmail.com>
2025-02-15 11:46:55 +0500

    refactor: change default value of isFull in RequestQueryDto from false to null for better clarity

--- src/Dto/AttributeGroup/RequestQueryDto.php
+++ src/Dto/AttributeGroup/RequestQueryDto.php
@@ -11,7 +11,7 @@ final readonly class RequestQueryDto
     public function __construct(
         #[Groups(['attribute_group:index'])]
         #[Assert\Type(type: ['bool', 'null'], groups: ['attribute_group:index'])]
-        public ?bool $isFull = false,
+        public ?bool $isFull = null,
         #[Groups(['attribute_group:index'])]
         #[Assert\Positive(groups: ['attribute_group:index'])]
         public ?int $page = 1,
